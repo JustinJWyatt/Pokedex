d513fdaeed05898278e23d55a66120ee01f60d9e
Justin Wyatt <justinjwyatt@gmail.com>
2021-02-22 11:42:19 -0600

    Added Android splashscreen and icon

---
+++ Pokedex/Pokedex/Pokedex.Android/SplashActivity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Pokedex.Droid
+{
+    [Activity(Theme = "@style/MyTheme.Splash", Icon = "@mipmap/pokeball", MainLauncher = true, NoHistory = true)]
+    public class SplashActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+    }
+}
