b9c30c212ec62b7847930da45d256e3fec5d2025
Justin Wyatt <justinjwyatt@gmail.com>
2021-02-23 00:37:24 -0600

    Added height converter

---
+++ Pokedex/Pokedex/Pokedex/Converters/DecimetersToFeetConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace Pokedex.Converters
+{
+    public class DecimetersToFeetConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return string.Format("{0} feet", Math.Round(((int)value) * 0.328084d, 1));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
